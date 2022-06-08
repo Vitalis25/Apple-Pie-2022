bcaa8729052a549da435f7a393891ca6b5d47205
Vitaly <lub-olen@yandex.ru>
2022-06-08 04:27:19 +0300

    Initial Commit

---
+++ Apple Pie 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie 2022
+//
+//  Created by Vitally Ochnev on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
